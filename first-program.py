48a1a35e757c549aab8845a60b6ac561400d6d83
Sunil Thapa <sunilthapa056@outlook.com>
2025-06-11 08:28:34 +0545

    added operators

--- first-program.py
+++ first-program.py
@@ -1,5 +1,12 @@
 try:
     age = int(input("Enter your age: "))
+    #operators
+    # == equals to
+    # != not equals to
+    # < less than
+    # > greater than
+    # <= less than equal to
+    # >= greater than equal to
     if age < 15:
         print(f"{age} is minor")
     elif age >= 15:
