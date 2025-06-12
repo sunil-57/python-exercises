369041ac4183cceb6478531d5fc62f2fd1c2b91a
Sunil Thapa <sunilthapa056@outlook.com>
2025-06-12 08:46:23 +0545

    working with strings

--- first-program.py
+++ first-program.py
@@ -1,11 +1,10 @@
-for i in range(2,6,2):
-    try:
-        age = int(input("Enter your age: "))
-        if age < 15:
-            print(f"{age} is minor")
-        elif age >= 15:
-            print(f"{age} is old")
-        else:
-            print("enter a proper age")
-    except:
-        print("please give numbers") 
+#strings in python
+sentence = '''strings in python'''
+print(sentence[8]) # accessing one element
+print(len(sentence)) # length of the string
+print(sentence[5:9]) # slicing
+print(sentence[11:])
+print(sentence[:5])
+print(sentence.split(" ")) #spliting
+for letter in sentence: #accessing each element
+    print(letter)
