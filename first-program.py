bd7fe31b138cd570b3778c79292153c904f07e40
Sunil Thapa <sunilthapa056@outlook.com>
2025-06-13 08:41:33 +0545

    removing elements

--- first-program.py
+++ first-program.py
@@ -3,9 +3,9 @@ name_list = ["ankit", "janak", "sunil"]
 name_tuple = ("ankit", "janak", "sunil")
 name_set = {"ankit", "janak", "sunil"}
 name_dictionary = {"ankit":24, "janak":23, "sunil":25}
-name_list.append(1)
-name_set.add("Krishna")
-name_dictionary["class"] = 20
+name_list.remove("sunil")
+name_set.remove("janak")
+del name_dictionary["ankit"]
 print(name_list)
 print(name_tuple)
 print(name_set)
