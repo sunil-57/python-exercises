a160f1f18206cc0f3483847efadc2746aae853aa
Sunil Thapa <sunilthapa056@outlook.com>
2025-06-13 08:13:29 +0545

    creating data structure

--- first-program.py
+++ first-program.py
@@ -1,10 +1,9 @@
-#strings in python
-sentence = '''strings in python'''
-print(sentence[8]) # accessing one element
-print(len(sentence)) # length of the string
-print(sentence[5:9]) # slicing
-print(sentence[11:])
-print(sentence[:5])
-print(sentence.split(" ")) #spliting
-for letter in sentence: #accessing each element
-    print(letter)
+#data structures
+name_list = ["ankit", "janak", "sunil"]
+name_tuple = ("ankit", "janak", "sunil")
+name_set = {"ankit", "janak", "sunil"}
+name_dictionary = {"ankit":24, "janak":23, "sunil":25}
+print(name_list)
+print(name_tuple)
+print(name_set)
+print(name_dictionary)
