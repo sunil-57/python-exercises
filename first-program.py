e6e79cbcb2906faaefa3263a0c0384a3a44b9e54
Sunil Thapa <sunilthapa056@outlook.com>
2025-06-13 08:46:19 +0545

    updating elements

--- first-program.py
+++ first-program.py
@@ -3,9 +3,8 @@ name_list = ["ankit", "janak", "sunil"]
 name_tuple = ("ankit", "janak", "sunil")
 name_set = {"ankit", "janak", "sunil"}
 name_dictionary = {"ankit":24, "janak":23, "sunil":25}
-name_list.remove("sunil")
-name_set.remove("janak")
-del name_dictionary["ankit"]
+name_list[2] = "Krishna"
+name_dictionary["sunil"] = 15
 print(name_list)
 print(name_tuple)
 print(name_set)
