c27f32a2695428b4a3ed74120a6a124d8bb5dc3d
Sunil Thapa <sunilthapa056@outlook.com>
2025-06-13 08:34:38 +0545

    adding elements

--- first-program.py
+++ first-program.py
@@ -1,10 +1,12 @@
 #data structures
-name_list = ["ankit", "janak", "sunil", "ankit"]
-name_tuple = ("ankit", "janak", "sunil", "ankit")
-name_set = {"ankit", "janak", "sunil", "ankit"}
-name_dictionary = {"ankit":24, "janak":23, "sunil":25, "ankit":24}
-print(name_list[3])
-print(name_tuple[2])
-for i in name_set:
-    print(i)
-print(name_dictionary['ankit'])
+name_list = ["ankit", "janak", "sunil"]
+name_tuple = ("ankit", "janak", "sunil")
+name_set = {"ankit", "janak", "sunil"}
+name_dictionary = {"ankit":24, "janak":23, "sunil":25}
+name_list.append(1)
+name_set.add("Krishna")
+name_dictionary["class"] = 20
+print(name_list)
+print(name_tuple)
+print(name_set)
+print(name_dictionary)
