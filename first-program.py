55d62c8407ee10daf6bcc118007d8b1f731f47f9
Sunil Thapa <sunilthapa056@outlook.com>
2025-06-16 08:53:06 +0545

    working with classes and objects

--- first-program.py
+++ first-program.py
@@ -1,21 +1,18 @@
-#to check the sum of two numbers 
-#provided by user is even or not using functions
-def add(a, b):
-    return a+b
-def check_even(number):
-    if number%2 == 0:
-        return True
-    else:
-        return False
+class Teacher:#creating a class
+    def __init__(self, name, age):#initialzing the attributes of the class
+        self.name = name
+        self.age = age
 
-#TODO ask two numbers from users
-first_number = int(input("Enter first number\n"))
-second_number = int(input("Enter second number\n"))
+    def give_assignment(self): #creating the behaviour of a class
+        print("Complete the assignment")
 
-#TODO find the sum using add function
-# sum = add(first_number, second_number)
-#TODO check the sum is even or not
-if check_even(add(first_number, second_number)):
-    print("Even")
-else:
-    print("Odd")
+
+#creating object of the class
+teacher1 = Teacher("Sunil", 25) 
+
+#accessing the attributes of class
+print(teacher1.name)
+print(teacher1.age)
+
+#calling the function of the class
+teacher1.give_assignment()
