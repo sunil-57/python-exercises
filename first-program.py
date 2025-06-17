ef7e5fe1ca4218d3395d3fe6752172203246f548
Sunil Thapa <sunilthapa056@outlook.com>
2025-06-17 07:41:25 +0545

    encapsulation

--- first-program.py
+++ first-program.py
@@ -1,8 +1,16 @@
 class Teacher:#creating a class
     def __init__(self, name, age):#initialzing the attributes of the class
-        self.name = name
-        self.age = age
+        self.__name = name  #making the attributes private
+        self.__age = age
 
+    #using setter methods to set the values in private attributes
+    def set_name(self, name):
+        self.__name = name
+    
+    #using getter methods to get the values of private attributes
+    def get_name(self):
+        return self.__name
+    
     def give_assignment(self): #creating the behaviour of a class
         print("Complete the assignment")
 
@@ -10,9 +18,11 @@ class Teacher:#creating a class
 #creating object of the class
 teacher1 = Teacher("Sunil", 25) 
 
-#accessing the attributes of class
-print(teacher1.name)
-print(teacher1.age)
+#accessing the private attributes of class is not allowed
+# print(teacher1.age)
+# print(teacher1.name)
 
 #calling the function of the class
 teacher1.give_assignment()
+teacher1.set_name("Janak")
+print(teacher1.get_name())
