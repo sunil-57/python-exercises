260cd658b2160b4f3012f1d06311f2b64ede431d
Sunil Thapa <sunilthapa056@outlook.com>
2025-06-18 08:37:53 +0545

    started with terminal user interface

--- smartphone-inventory-ms/inventory_app.py
+++ smartphone-inventory-ms/inventory_app.py
@@ -1,33 +1,45 @@
 from phone import Phone
 
 #TODO how do i implement my inventory?
-phone_inventory = []
+phone_inventory = [Phone("model","brand",223, "storage", 23)]
 
 
 #TODO add a phone in inventory
 #TODO ask the phone details from user before adding the phone to inventory
-#TODO 
-model = input("Enter the phone model: ")
-brand = input("Enter the phone brand: ")
-price = int(input("Enter the phone price: "))
-storage = input("Enter the phone storage: ")
-quantity = int(input("Enter the phone quantity: "))
+#TODO need to handle exceptions
+def add_phone():
+    model = input("Enter the phone model: ")
+    brand = input("Enter the phone brand: ")
+    price = int(input("Enter the phone price: "))
+    storage = input("Enter the phone storage: ")
+    quantity = int(input("Enter the phone quantity: "))
 
-phone = Phone(model,brand, price, storage, quantity)
+    phone = Phone(model,brand, price, storage, quantity)
 
-phone_inventory.append(phone)
+    phone_inventory.append(phone)
 
 # print(phone_inventory)
 
 
 #TODO view details of a phone
 #TODO how do i show information of the phone that the user wants?
-for phone in phone_inventory:
-    print(f"Phone Model: {phone.get_model()}")
-    print(f"Phone Brand: {phone.get_brand()}")
+def view_phone_details():
+    for phone in phone_inventory:
+        print(f"Phone Model: {phone.get_model()}")
+        print(f"Phone Brand: {phone.get_brand()}")
 
 
 #TODO update detail of phones
 #TODO delete a phone
 
 #TODO how to allow users to do the operations?
+#TODO how to let the user use the program as much as they want?
+#TODO how to let the user exit the program?
+print("Enter 1 to add phone: ")
+print("Enter 2 to view phone detail: ")
+option = int(input("Choose an option: "))
+if(option == 1):
+    add_phone()
+elif(option == 2):
+    view_phone_details()
+
