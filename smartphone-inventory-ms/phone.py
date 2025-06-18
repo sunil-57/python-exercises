27cb9cde9daa4845b32325c66bf7ac898af193f2
Sunil Thapa <sunilthapa056@outlook.com>
2025-06-18 07:18:28 +0545

    added setters and getters

--- smartphone-inventory-ms/phone.py
+++ smartphone-inventory-ms/phone.py
@@ -8,6 +8,35 @@ class Phone:
         self.__price = price
         self.__storage = storage
         self.__quantity = quantity
-        
     
+    
+    def set_model(self, model):
+        self.__model = model
         
+    def set_brand(self, brand):
+        self.__brand = brand
+        
+    def set_price(self, price):
+        self.__price = price
+        
+    def set_storage(self, storage):
+        self.__storage = storage
+        
+    def set_qunatity(self, quantity):
+        self.__quantity = quantity
+    
+    
+    def get_model(self):
+       return self.__model
+   
+    def get_brand(self):
+       return self.__brand
+   
+    def get_price(self):
+       return self.__price
+   
+    def get_storage(self):
+       return self.__storage
+   
+    def get_qunatity(self):
+       return self.__quantity        
