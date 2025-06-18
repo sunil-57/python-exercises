12390dd549065f04acd1e978e0948c5ac06df30f
Sunil Thapa <sunilthapa056@outlook.com>
2025-06-18 07:53:13 +0545

    add phone in list

---
+++ smartphone-inventory-ms/inventory_app.py
@@ -0,0 +1,24 @@
+from phone import Phone
+
+#TODO how do i implement my inventory?
+phone_inventory = []
+
+
+#TODO add a phone in inventory
+#TODO ask the phone details from user before adding the phone to inventory
+phone = Phone("S20","Samsung", 50000, "32 GB", 30 )
+phone1 = Phone("S23","Samsung", 70000, "32 GB", 30 )
+phone_inventory.append(phone)
+phone_inventory.append(phone1)
+
+print(phone_inventory)
+
+
+#TODO view details of a phone
+
+
+
+#TODO update detail of phones
+#TODO delete a phone
+
+#TODO how to allow users to do the operations?
